6bd2ca4498eff373adc0319fd569d7af36bf3531
cesarluisg <cesarluisg@gmail.com>
2025-03-24 21:53:31 -0300

    main inicial

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Selector de Diapositiva según receta seleccionada en PLC")
+print("Versión 1.0")
+print("Desarrollado por: César Luis Guzmán - CICAL")
+print("Fecha: 2025-03-24")
+
+# Importar librerías
